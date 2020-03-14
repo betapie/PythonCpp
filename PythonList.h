65ed605f681329e37d1dd1f71c1b03f579b1c0ea
Manuel Lauterbach <ma.lauterbach@gmail.com>
2020-03-14 23:38:26 +0100

    Fixes on PythonList and castings

--- PythonList.h
+++ PythonList.h
@@ -1,18 +1,21 @@
 #pragma once
 #include "PythonTypeTraits.h"
 #include "PythonObject.h"
-#include <typeinfo>
+#include <initializer_list>
+
+// WARNING: THIS IS NOWHERE NEAR FEATURE COMPLETE!
 
 namespace pycpp
 {
     /*
         PythonList will can be created of a single type. If you want to use multiple types,
-        use the generic PythonObject as template parameter
+        use the generic PythonObject as template parameter. For the time being, do not do that
+        as it's not safe
     */
     template<typename T>
     class PythonList : public PythonObject
     {
-        static_assert(isPythonBaseType_v<T>, "PythonList T not valid PythonBaseType");
+        static_assert(isPythonBaseType_v<T>, "PythonList<T>: T is not a valid PythonBaseType");
     public:
         // Default constructor will create a new PythonList with size 0
         // analog to myList = [] or myList = List() in Python
@@ -23,11 +26,30 @@ namespace pycpp
                 throw PythonError();
         }
 
-        // Create a Python List of size sizeof(Args...) with the contents of args...
-        template<typename... Args> // TODO enable_if Args... are convertible to T
-        PythonList(Args... args)
-        {
+        // Variadic Constructor disabled atm. Use the initializer_list overload
 
+        //// Create a Python List of size sizeof(Args...) with the contents of args...
+        //template<typename... Args> // TODO enable_if Args... are convertible to T
+        //PythonList(Args... args)
+        //{
+        //    m_pObject = PyList_New(sizeof(Args...));
+        //    // Initialize the values
+        //}
+
+        PythonList(const std::initializer_list<T>& iList)
+        {
+            m_pObject = PyList_New(iList.size());
+            if (!m_pObject)
+                throw PythonError();
+            size_t idx = 0;
+            for (const auto& elem : iList)
+            {
+                auto newElem = ToPythonObject(elem);
+                Py_INCREF(newElem.get()); // PyList_SetItem steals a ref..
+                if (PyList_SetItem(m_pObject, idx, newElem.get()) == -1)
+                    throw PythonError();
+                ++idx;
+            }
         }
 
         // Take ownership of an existing PyObject which points to a Python List or subtype of List
@@ -39,9 +61,45 @@ namespace pycpp
                 throw PythonError("PyObject not of List type"); // TODO more info
         }
 
-        // Copy / Move Construct and Assign
+        PythonList(const PythonList& other)
+            :PythonObject(other)
+        {}
 
-        [[nodiscard]] size_t size() noexcept
+        PythonList& operator=(const PythonList& other)
+        {
+            PythonObject::operator=(other);
+            return *this;
+        }
+
+        PythonList(PythonList&& other) noexcept
+            :PythonObject(std::move(other))
+        {}
+
+        PythonList& operator=(PythonList&& other) noexcept
+        {
+            PythonObject::operator=(std::move(other));
+            return *this;
+        }
+
+        PythonList(const PythonObject& other)
+            :PythonObject(other)
+        {
+            if (PyList_Check(m_pObject) == 0)
+                throw PythonError("PyObject not of List type"); // TODO more info
+        }
+
+        PythonList& operator=(const PythonObject& other)
+        {
+            if (PyList_Check(other.m_pObject) == 0)
+                throw PythonError("PyObject not of List type"); // TODO more info
+            PythonObject::operator=(other);
+            return *this;
+        }
+
+        // Note: No move construction/assignment from PythonObject, because due to the PyList_Check
+        // they cannot be defined noexcept!
+
+        [[nodiscard]] size_t size() const noexcept
         {
             return PyList_Size(m_pObject); // can this fail in any way?
         }
@@ -52,9 +110,9 @@ namespace pycpp
 
         void append(const T& val)
         {
-            // convert val to PyObject of type T
+            const auto py_val = ToPythonObject(val);
 
-            if (PyList_Append(m_pObject, nullptr) == -1)
+            if (PyList_Append(m_pObject, py_val.get()) == -1)
                 throw PythonError();
         }
 
@@ -72,9 +130,9 @@ namespace pycpp
 
         void insert(size_t index, const T& val)
         {
-            // convert val to PyObject
+            const auto py_val = ToPythonObject(val);
 
-            if (PyList_Insert(m_pObject, index, nullptr) == -1)
+            if (PyList_Insert(m_pObject, index, py_val.get()) == -1)
                 throw PythonError();
         }
 
@@ -92,12 +150,14 @@ namespace pycpp
 
         void sort()
         {
-            // TODO
+            if (PyList_Sort(m_pObject) == -1)
+                throw PythonError();
         }
 
         void reverse()
         {
-            // TODO
+            if (PyList_Reverse(m_pObject) == -1)
+                throw PythonError();
         }
 
     private:
--- PythonTypeTraits.h
+++ PythonTypeTraits.h
@@ -115,12 +115,76 @@ namespace pycpp
     // python_cast converts the contents of a PythonObject to your desired type, if possible.
     // for example a conversion to long will succeed, if the Python object pointed to is of type int or has
     // __int__() implemented (can be converted to int). This cast will also check for possible overflow
+    // note: Python C API allows for conversion from int to double etc. These will not be supported.
+    // Please cast them accordingly and if you need conversions cast them manually
     template<typename T> // base template, this will not do anything except warning about wrong types
     [[nodiscard]] T python_cast(const PythonObject& pyObj)
     {
         static_assert(isPythonBaseType_v<T>, "python_cast: Type not supported");
         return T{};
     }
+
+    template<>
+    [[nodiscard]] bool python_cast<bool>(const PythonObject& pyObj)
+    {
+        const auto check = PyObject_IsTrue(pyObj.get());
+        if (check == -1)
+            throw PythonError();
+        if (check == 1)
+            return true;
+        return false;
+    }
+
+    template<>
+    [[nodiscard]] long python_cast<long>(const PythonObject& pyObj)
+    {
+        const auto ret = PyLong_AsLong(pyObj.get());
+        if (PyErr_Occurred())
+            throw PythonError();
+
+        return ret;
+    }
+
+    template<>
+    [[nodiscard]] unsigned long python_cast<unsigned long>(const PythonObject& pyObj)
+    {
+        const auto ret = PyLong_AsUnsignedLong(pyObj.get());
+        if (PyErr_Occurred())
+            throw PythonError();
+
+        return ret;
+    }
+
+    template<>
+    [[nodiscard]] long long python_cast<long long>(const PythonObject& pyObj)
+    {
+        const auto ret = PyLong_AsLongLong(pyObj.get());
+        if (PyErr_Occurred())
+            throw PythonError();
+
+        return ret;
+    }
+
+    template<>
+    [[nodiscard]] unsigned long long python_cast<unsigned long long>(const PythonObject& pyObj)
+    {
+        const auto ret = PyLong_AsUnsignedLongLong(pyObj.get());
+        if (PyErr_Occurred())
+            throw PythonError();
+
+        return ret;
+    }
+
+    template<>
+    [[nodiscard]] double python_cast<double>(const PythonObject& pyObj)
+    {
+        const auto ret = PyFloat_AsDouble(pyObj.get());
+        if (PyErr_Occurred())
+            throw PythonError();
+
+        return ret;
+    }
+
 
     // TODO: implement unchecked_python_cast and forced_python_cast
 }
