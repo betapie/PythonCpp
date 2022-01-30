be04c8d7e1e8ea65a0099f7b7acd62dbe4fe0ac6
betapie <ma.lauterbach@gmail.com>
2022-01-30 18:35:53 +0100

    Tidied up PyCppDefines for GNU C

--- include/PythonCpp/PyCppDefines.h
+++ include/PythonCpp/PyCppDefines.h
@@ -6,10 +6,6 @@
     #else
         #define PYCPP_API __declspec(dllimport)
     #endif
-#elif defined __GNUC__ && __GNUC__ >= 4 || defined(__APPLE__))
-    #ifdef PYCPP_EXPORTS
-        #define PYCPP_API __attribute__ ((visibility ("default")))
-    #endif
 #else
     #define PYCPP_API
 #endif
